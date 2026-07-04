55d0c442536a12e8873fbdb1391098a50504825e
Chadrackbanza <chadrackbanza244@gmail.com>
2026-07-04 06:15:44 +0200

    Correction finale : sécurité Pod et chiffrement S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "mon-bucket-prive-5g"
 }
 
-# CORRECTION : Blocage de tout accès public au bucket
+# 1. Blocage de tout accès public (Déjà présent)
 resource "aws_s3_bucket_public_access_block" "b_access_block" {
   bucket = aws_s3_bucket.b.id
 
@@ -14,4 +14,13 @@ resource "aws_s3_bucket_public_access_block" "b_access_block" {
   restrict_public_buckets = true
 }
 
-# Suppression de la ressource aws_s3_bucket_acl qui créait la faille
+# 2. Ajout du chiffrement côté serveur (Correction AWS-0132)
+resource "aws_s3_bucket_server_side_encryption_configuration" "b_encryption" {
+  bucket = aws_s3_bucket.b.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
